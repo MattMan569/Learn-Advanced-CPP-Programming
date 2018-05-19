c1532137a299ed882660e00c46df58209bd9344d
Matthew Polsom <matthewpolsom@hotmail.com>
2018-05-19 09:09:28 -0600

    Completed

--- STL/Vectors/Vectors.cpp
+++ STL/Vectors/Vectors.cpp
@@ -1,12 +1,50 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
 using namespace std;
 
+
 int main()
 {
+	vector<string> strings;
+
+	strings.push_back("one");
+	strings.push_back("two");
+	strings.push_back("three");
+
+	// Subscript
+	for (int i = 0; i < strings.size(); i++)
+	{
+		cout << strings[i] << endl;
+	}
+
+	cout << endl;
+
+	// Explicit iterator
+	for (vector<string>::iterator it = strings.begin(); it != strings.end(); it++)
+	{
+		cout << *it << endl;
+	}
+
+	cout << endl;
+
+	// Auto iterator
+	for (auto it = strings.begin(); it != strings.end(); it++)
+	{
+		cout << *it << endl;
+	}
+
+	cout << endl;
+
+	// Range
+	for (auto const &value : strings)
+	{
+		cout << value << endl;
+	}
 
 
 
+	cout << endl;
 	return 0;
 }
