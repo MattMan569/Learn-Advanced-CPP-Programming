b6a4b0601dd9b22adbb63290a444e482420eb974
Matthew Polsom <matthewpolsom@hotmail.com>
2018-05-18 23:12:32 -0600

    Completed

--- Files/ReadingAndWritingBinaryFiles/ReadingAndWritingBinaryFiles.cpp
+++ Files/ReadingAndWritingBinaryFiles/ReadingAndWritingBinaryFiles.cpp
@@ -11,7 +11,7 @@ struct Person
 {
 	char name[50];
 	int age;
-	double weight;
+	double height;
 };
 
 // Re-enable padding
@@ -19,7 +19,55 @@ struct Person
 
 int main()
 {
+	Person someone = { "Matthew", 21, 1.85 };
 
+
+	string fileName = "test.bin";
+
+	/// Write binary file
+
+	// ofstream outputFile;
+	fstream outputFile;
+
+	// Open the file for writing in binary
+	// outputFile.open(fileName, ios::binary);
+	outputFile.open(fileName, ios::binary | ios::out);
+
+	if (outputFile.is_open())
+	{
+		outputFile.write(reinterpret_cast<char *>(&someone), sizeof(Person));
+
+		outputFile.close();
+	}
+	else
+	{
+		cout << "Could not create file: " << fileName << endl;
+	}
+
+
+	/// Read binary file
+
+	// Create empty instance
+	Person someoneElse = {};
+
+	ifstream inputFile;
+
+	// Open the file for reading in binary
+	inputFile.open(fileName, ios::binary);
+
+	if (inputFile.is_open())
+	{
+		// Read the binary file into the new Person instance
+		inputFile.read(reinterpret_cast<char *>(&someoneElse), sizeof(Person));
+
+		inputFile.close();
+	}
+	else
+	{
+		cout << "Could not read file: " << fileName << endl;
+	}
+
+	cout << someoneElse.name << ". " << someoneElse.age << ". " << someoneElse.height << endl;
 
 
 	return 0;
