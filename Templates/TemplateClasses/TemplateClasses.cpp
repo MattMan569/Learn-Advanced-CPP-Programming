d470fb2910962267104f072b6150cb4ac2fbb1cc
Matthew Polsom <matthewpolsom@hotmail.com>
2018-05-25 09:35:03 -0600

    Created template class project

---
+++ Templates/TemplateClasses/TemplateClasses.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+template<class T, class K>
+class Test
+{
+private:
+	T obj;
+
+public:
+	Test(T obj)
+	{
+		this->obj = obj;
+	}
+
+	void print() const
+	{
+		cout << obj << endl;
+	}
+};
+
+
+int main()
+{
+	Test<string, int> test1("Hello");
+	test1.print();
+
+
+	return 0;
+}
