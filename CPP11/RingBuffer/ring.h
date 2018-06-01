38da86e7b5ecc5d2fec5a2ff347705dc133fce2a
Matthew Polsom <matthewpolsom@hotmail.com>
2018-06-01 14:01:45 -0600

    Added iterator

--- CPP11/RingBuffer/ring.h
+++ CPP11/RingBuffer/ring.h
@@ -13,6 +13,10 @@ private:
 	T * m_values;
 
 public:
+	// Declare iterator as a sub type of ring
+	class iterator;
+
+
 	ring(int size) : m_pos(0), m_size(size), m_values(NULL)
 	{
 		m_values = new T[size];
@@ -22,6 +26,21 @@ public:
 	{
 		delete[] m_values;
 	}
+
+
+	iterator begin()
+	{
+		// Specify pos 0, pass the ring object to the iterator
+		// to gain access to be able to refer to member variables
+		return iterator(0, *this);
+	}
+
+	iterator end()
+	{
+		// Specify last position
+		return iterator(m_size, *this);
+	}
+
 
 	int size()
 	{
@@ -46,10 +65,6 @@ public:
 	{
 		return m_values[pos];
 	}
-
-
-	// Declare iterator as a sub type of ring
-	class iterator;
 };
 
 
@@ -58,10 +73,41 @@ public:
 template <class T>
 class ring<T>::iterator
 {
+private:
+	int m_pos;
+	ring & m_ring;
+
 public:
-	void print()
+	iterator(int pos, ring & buffer) : m_pos(pos), m_ring(buffer)
 	{
-		cout << "Hello from iterator" << endl;
+		
+	}
+
+
+	// Specifying a type denotes this as the postfix version of the ++ operator
+	iterator & operator++(int)
+	{
+		m_pos++;
+		return *this;
+	}
+
+	// Prefix
+	iterator & operator++()
+	{
+		m_pos++;
+		return *this;
+	}
+
+	// Return the data from this iterator's position
+	T & operator*()
+	{
+		return m_ring.get(m_pos);
+	}
+
+	bool operator!=(const iterator & other) const
+	{
+		// Iterators are not equal if their positions are not equal
+		return m_pos != other.m_pos;
 	}
 };
 
